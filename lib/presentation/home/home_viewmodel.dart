eb70a8bf147b37a303606f94e16744df4de0c30f
Shota Kinoshita <mashota.88@gmail.com>
2026-02-23 20:09:44 +0900

    FutureProviderからasyncNotifierProviderに

--- lib/presentation/home/home_viewmodel.dart
+++ lib/presentation/home/home_viewmodel.dart
@@ -2,6 +2,21 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:yd_app/data/repositories/message_repository.dart';
 import 'package:yd_app/domain/entities/message.dart';
 
-final messagesProvider = FutureProvider<List<Message>>((ref) {
+final messagesProvider2 = FutureProvider<List<Message>>((ref) {
   return ref.watch(messageRepositoryProvider).fetchMessages();
 });
+
+final messagesProvider = AsyncNotifierProvider<MessageNotifier, List<Message>>(MessageNotifier.new);
+
+class MessageNotifier extends AsyncNotifier<List<Message>> {
+  @override
+  Future<List<Message>> build() async {
+    final messages = await ref.watch(messageRepositoryProvider).fetchMessages();
+    return messages;
+  }
+}
+
+
+
+
+
